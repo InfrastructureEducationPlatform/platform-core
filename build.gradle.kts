54438965b35e48a839afe68ee8fdd6865847e940
KangDroid <hyunwoo286@gmail.com>
2023-08-13 16:50:35 +0900

    [Feature] Initiate Liquibase

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.spring") version "1.8.22"
     kotlin("plugin.jpa") version "1.8.22"
+    id("org.liquibase.gradle") version "2.2.0"
 }
 
 group = "com.infrastructure.education"
@@ -19,6 +20,32 @@ java {
 configurations {
     compileOnly {
         extendsFrom(configurations.annotationProcessor.get())
+    }
+
+    liquibase {
+        activities.register("main") {
+            val dbHostWithPort = project.ext.properties.get("dbHost")
+            val dbName = project.ext.properties.get("dbName")
+            val dbUsername = project.ext.properties.get("dbUsername")
+            val dbPassword = project.ext.properties.get("dbPassword")
+            val dbJdbcUrl = "jdbc:postgresql://${dbHostWithPort}/${dbName}"
+
+            this.arguments = mapOf(
+                    "changelogFile" to "src/main/resources/db/db.changelog-master.yml",
+                    "url" to dbJdbcUrl,
+                    "username" to dbUsername,
+                    "password" to dbPassword,
+                    "driver" to "org.postgresql.Driver",
+                    "classpath" to "src/main/resources",
+                    "referenceUrl" to "hibernate:spring:com.infrastructure.education?dialect=org.hibernate.dialect.PostgreSQL10Dialect&hibernate.physical_naming_strategy=org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy&hibernate.implicit_naming_strategy=org.springframework.boot.orm.jpa.hibernate.SpringImplicitNamingStrategy",
+                    "referenceDriver" to "liquibase.ext.hibernate.database.connection.HibernateDriver"
+            )
+        }
+        runList = "main"
+    }
+
+    liquibaseRuntime {
+        extendsFrom(configurations.compileClasspath.get())
     }
 }
 
@@ -53,6 +80,15 @@ dependencies {
 
     val mockkVersion = "1.13.5"
     testImplementation("io.mockk:mockk:${mockkVersion}")
+
+    // Liquibase
+    liquibaseRuntime("org.liquibase:liquibase-core:4.23.1")
+    liquibaseRuntime("org.liquibase:liquibase-groovy-dsl:3.0.3")
+    liquibaseRuntime("org.postgresql:postgresql")
+    liquibaseRuntime("org.liquibase.ext:liquibase-hibernate6:4.23.0")
+    liquibaseRuntime("org.yaml:snakeyaml:2.0")
+    liquibaseRuntime("info.picocli:picocli:4.6.1")
+    liquibaseRuntime(sourceSets.getByName("main").output)
 }
 
 tasks.withType<KotlinCompile> {
