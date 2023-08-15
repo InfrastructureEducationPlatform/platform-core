107cadb539090b191bbd747ac31d95964895f4b6
KangDroid <hyunwoo286@gmail.com>
2023-08-15 22:43:45 +0900

    [Fix] Fix Status code in ErrorResponse is being deserialized as string

--- src/main/kotlin/com/infrastructure/education/common/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/infrastructure/education/common/GlobalExceptionHandler.kt
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 class GlobalExceptionHandler {
     @ExceptionHandler(ApiException::class)
     fun handleApiException(apiException: ApiException): ResponseEntity<ErrorResponse> {
-        val errorResponse = ErrorResponse(apiException.statusCode, apiException.errorMessage, apiException.errorTitle)
+        val errorResponse = ErrorResponse(apiException.statusCode.value(), apiException.errorMessage, apiException.errorTitle)
         return ResponseEntity<ErrorResponse>(errorResponse, apiException.statusCode)
     }
 
     @ExceptionHandler(Exception::class)
     fun handleGlobalException(exception: Exception): ResponseEntity<ErrorResponse> {
-        return ResponseEntity<ErrorResponse>(ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Unknown Error occurred", ErrorTitle.UNKNOWN_INTERNAL_ERROR), HttpStatus.INTERNAL_SERVER_ERROR)
+        return ResponseEntity<ErrorResponse>(ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Unknown Error occurred", ErrorTitle.UNKNOWN_INTERNAL_ERROR), HttpStatus.INTERNAL_SERVER_ERROR)
     }
 }
--- src/main/kotlin/com/infrastructure/education/common/models/responses/ErrorResponse.kt
+++ src/main/kotlin/com/infrastructure/education/common/models/responses/ErrorResponse.kt
@@ -1,10 +1,9 @@
 package com.infrastructure.education.common.models.responses
 
 import com.infrastructure.education.common.ErrorTitle
-import org.springframework.http.HttpStatusCode
 
 class ErrorResponse(
-        val statusCode: HttpStatusCode,
+        val statusCode: Int,
         val errorMessage: String,
         val errorTitle: ErrorTitle
 )
