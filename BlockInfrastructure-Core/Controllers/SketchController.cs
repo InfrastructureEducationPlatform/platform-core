e7dd52812051fc7e37291840e1ce476272a7a076
KangDroid <hyunwoo286@gmail.com>
2023-11-13 22:26:54 +0900

    [Improvement] SketchController에 대한 Swagger API Documentation 추가 작성

--- BlockInfrastructure-Core/Controllers/SketchController.cs
+++ BlockInfrastructure-Core/Controllers/SketchController.cs
@@ -10,6 +10,17 @@ namespace BlockInfrastructure.Core.Controllers;
 [Route("/channels/{channelId}/sketches")]
 public class SketchController(SketchService sketchService) : ControllerBase
 {
+    /// <summary>
+    ///     채널 내에 있는 모든 스케치를 가져옵니다,
+    /// </summary>
+    /// <remarks>
+    ///     이 API는 채널의 Owner, Reader 모두 조회할 수 있습니다.
+    /// </remarks>
+    /// <param name="channelId">조회할 채널 ID</param>
+    /// <returns></returns>
+    /// <response code="200">조회에 성공한 경우.</response>
+    /// <response code="401">인증 토큰이 없어 인증에 실패한 경우</response>
+    /// <response code="403">스케치를 가져오는데 채널 권한이 부족한 경우(해당 API에서는 소속되어 있지 않은 채널을 조회하려 했을 때 반환)</response>
     [HttpGet]
     [ChannelRole(ChannelPermissionType.Owner, ChannelPermissionType.Reader)]
     public async Task<IActionResult> ListSketchesInChannelAsync(string channelId)
