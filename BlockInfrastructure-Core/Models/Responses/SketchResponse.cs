1d96639b49235d457379a91309845e41be7af40b
KangDroid <hyunwoo286@gmail.com>
2023-11-13 10:39:56 +0900

    [Improvement] Swagger Schema Document 강화

--- BlockInfrastructure-Core/Models/Responses/SketchResponse.cs
+++ BlockInfrastructure-Core/Models/Responses/SketchResponse.cs
@@ -1,12 +1,38 @@
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json;
 
 namespace BlockInfrastructure.Core.Models.Responses;
 
 public class SketchResponse
 {
+    /// <summary>
+    ///     스케치의 Unique ID입니다.
+    /// </summary>
+    [Required]
     public string SketchId { get; set; }
+
+    /// <summary>
+    ///     스케치의 이름입니다.
+    /// </summary>
+    [Required]
     public string Name { get; set; }
+
+    /// <summary>
+    ///     스케치의 설명입니다.
+    /// </summary>
+    [Required]
     public string Description { get; set; }
+
+    /// <summary>
+    ///     스케치의 채널 ID입니다.
+    /// </summary>
+    [Required]
     public string ChannelId { get; set; }
+
+
+    /// <summary>
+    ///     스케치의 블록 스케치입니다.
+    /// </summary>
+    [Required]
     public JsonDocument BlockSketch { get; set; }
 }
